0fb78a1e606549e4d5fe53e36c7fd516f55178fe
Daniel Amorim <dan-amorim@live.com>
2022-08-07 21:34:54 -0300

    Add seleção de departamento e deleção de vendedor

--- VendasWebMvc/Services/VendedorServico.cs
+++ VendasWebMvc/Services/VendedorServico.cs
@@ -25,5 +25,17 @@ namespace VendasWebMvc.Services
             _context.Add(vendedor);
             _context.SaveChanges();
         }
+
+        public Vendedor FindById(int id)
+        {
+            return _context.Vendedor.FirstOrDefault(vendedor => vendedor.Id == id);
+        }
+
+        public void Remove(int id)
+        {
+            var obj = _context.Vendedor.Find(id);
+            _context.Vendedor.Remove(obj);
+            _context.SaveChanges();
+        }
     }
 }
